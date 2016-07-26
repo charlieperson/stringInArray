8f0671387102bf51503716a4e599af0e0771ce21
charlieperson <charlie.person@hyperisland.se>
2016-07-26 09:28:59 -0500

    Solves stringInArray using recursion

--- stringInArray.js
+++ stringInArray.js
@@ -1,7 +1,21 @@
+// for loop
+
 function searchString (str, array) {
   var status = false;
   for(var i=0; i<array.length; i++) {
     if(array[i] === str) status = true;
   }
   return status;
+}
+
+// recursion
+
+function searchString(str, array) {
+  var i = 0;
+  function findIt(item) {
+    if(item === str) return true;
+    if(i >= array.length) return false;
+    return findIt(array[i= i+1]);
+  }
+  return findIt(array[0]);
 }
